434941e9e07e65ec53e26071eca1635fdcdd2752
Avi Mishra <aviam2425@gmail.com>
2026-03-21 14:41:07 +0530

    Frontend setup with React

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 CareerCompass AI</h1>
+      <p>Upload your resume and get career insights</p>
+    </div>
+  );
+}
+
+export default App;
